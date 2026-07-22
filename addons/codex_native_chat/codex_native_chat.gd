c350b4030e5f74d08c04ed7ace9049f433cec4ab
Leon <45938585+sn-Cloud@users.noreply.github.com>
2026-07-22 11:16:45 +0800

    implement Codex editor dock lifecycle

--- addons/codex_native_chat/codex_native_chat.gd
+++ addons/codex_native_chat/codex_native_chat.gd
@@ -1,15 +1,19 @@
 @tool
 extends EditorPlugin
 
-const DOCK_SCENE = preload("res://addons/codex_native_chat/codex_native_chat_dock.tscn")
+const DOCK_SCENE := preload("res://addons/codex_native_chat/codex_native_chat_dock.tscn")
 
-var dock: Control
+var _dock: CodexNativeChatDock
 
-func _enter_tree():
-	dock = DOCK_SCENE.instantiate()
-	add_control_to_dock(DOCK_SLOT_RIGHT_BL, dock)
+func _enter_tree() -> void:
+	_dock = DOCK_SCENE.instantiate() as CodexNativeChatDock
+	_dock.configure(get_editor_interface())
+	add_control_to_dock(DOCK_SLOT_RIGHT_BL, _dock)
 
-func _exit_tree():
-	if dock:
-		remove_control_from_docks(dock)
-		dock.queue_free()
+func _exit_tree() -> void:
+	if _dock == null:
+		return
+	_dock.shutdown()
+	remove_control_from_docks(_dock)
+	_dock.queue_free()
+	_dock = null
